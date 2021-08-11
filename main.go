eed25f831a20960a669c34914097f2abce98de50
hidebon0630 <hyagi0630@icloud.com>
2021-08-11 15:14:23 +0900

    バッファ

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "os"
+import (
+	"bytes"
+	"fmt"
+)
 
 func main() {
-	os.Stdout.Write([]byte("os.Stdout example\n"))
+	var buffer bytes.Buffer
+	buffer.Write([]byte("bytes.Buffer example\n"))
+	fmt.Println(buffer.String())
 }
