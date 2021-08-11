922ef0fc3e7dae9768c1adf8394a9c9965f1b28d
hidebon0630 <hyagi0630@icloud.com>
2021-08-11 22:36:42 +0900

    クロージャに変更

--- main.go
+++ main.go
@@ -5,16 +5,13 @@ import (
 	"time"
 )
 
-// 新しく作られるgoroutineが呼ぶ関数
-func sub() {
-	fmt.Println("sub() is running")
-	time.Sleep(time.Second)
-	fmt.Println("sub() is finished")
-}
-
 func main() {
 	fmt.Println("start sub()")
-	// goroutineを作って関数を実行
-	go sub()
+	// インラインで無名関数を作ってその場でgoroutineで実行
+	go func() {
+		fmt.Println("sub() is running")
+		time.Sleep(time.Second)
+		fmt.Println("sub() is finished")
+	}()
 	time.Sleep(2 * time.Second)
 }
