e141821a6bc54270fcc4f6e13764872f2188d7b3
hidebon0630 <hyagi0630@icloud.com>
2021-08-11 18:33:54 +0900

    バイナリフォーマットのpngファイルを分析

--- main.go
+++ main.go
@@ -1,27 +1,51 @@
 package main
 
 import (
-	"bufio"
+	"encoding/binary"
 	"fmt"
 	"io"
-	"net"
-	"net/http"
 	"os"
 )
 
+func dumpChunk(chunk io.Reader) {
+	var length int32
+	binary.Read(chunk, binary.BigEndian, &length)
+	buffer := make([]byte, 4)
+	chunk.Read(buffer)
+	fmt.Printf("chunk '%v' (%d bytes)\n", string(buffer), length)
+}
+
+func readChunks(file *os.File) []io.Reader {
+	// チャンクを格納する配列
+	var chunks []io.Reader
+
+	// 最初の8バイトを飛ばす(シグネチャの8バイト)
+	file.Seek(8, 0)
+	var offset int64 = 8
+
+	for {
+		var length int32
+		err := binary.Read(file, binary.BigEndian, &length)
+		if err == io.EOF {
+			break
+		}
+		chunks = append(chunks, io.NewSectionReader(file, offset, int64(length)+12))
+		// 次のチャンクの先頭に移動
+		// 現在位置は長さを読み終わった箇所なので
+		// チャンク名(4バイト) + データ長 + CRC(4バイト)先に移動
+		offset, _ = file.Seek(int64(length+8), 1)
+	}
+	return chunks
+}
+
 func main() {
-	conn, err := net.Dial("tcp", "ascii.jp:80")
+	file, err := os.Open("Lenna.png")
 	if err != nil {
 		panic(err)
 	}
-	conn.Write([]byte("GET / HTTP/1.0\r\nHost: ascii.jp\r\n\r\n"))
-	res, err := http.ReadResponse(bufio.NewReader(conn), nil)
-	if err != nil {
-		panic(err)
+	defer file.Close()
+	chunks := readChunks(file)
+	for _, chunk := range chunks {
+		dumpChunk(chunk)
 	}
-	// ヘッダーを表示してみる
-	fmt.Println(res.Header)
-	// ボディーを表示してみる。最後にはClose()すること
-	defer res.Body.Close()
-	io.Copy(os.Stdout, res.Body)
 }
