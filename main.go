24e0617be63c14a1480c0c41b27a80f8fb8472d0
hidebon0630 <hyagi0630@icloud.com>
2021-08-11 17:58:51 +0900

    ネットワーク通信の読み込み

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"io"
+	"net"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("file.go")
+	conn, err := net.Dial("tcp", "ascii.jp:80")
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-	io.Copy(os.Stdout, file)
+	conn.Write([]byte("GET / HTTP/1.0\r\nHost: ascii.jp\r\n\r\n"))
+	io.Copy(os.Stdout, conn)
 }
