451ee7fee93cd99fd3c02e8cb63f1ecd09ed8d84
hidebon0630 <hyagi0630@icloud.com>
2021-08-12 14:35:00 +0900

    コンテキスト

--- main.go
+++ main.go
@@ -1,36 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"math"
 )
 
-func primeNumber() chan int {
-	result := make(chan int)
-	go func() {
-		result <- 2
-		for i := 3; i < 100000; i += 2 {
-			l := int(math.Sqrt(float64(i)))
-			found := false
-			for j := 3; j < l+1; j += 2 {
-				if i%j == 0 {
-					found = true
-					break
-				}
-			}
-			if !found {
-				result <- i
-			}
-		}
-		close(result)
-	}()
-	return result
-}
-
 func main() {
-	pn := primeNumber()
-	// ここがポイント
-	for n := range pn {
-		fmt.Println(n)
-	}
+	fmt.Println("start sub()")
+	// 終了を受け取るための終了関数付きコンテキスト
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		fmt.Println("sub() is finished")
+		// 終了を通知
+		cancel()
+	}()
+	// 終了を待つ
+	<-ctx.Done()
+	fmt.Println("all tasks are finished")
 }
