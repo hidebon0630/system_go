62e697c4583f9b5e08c41f525df7d996a8a97cdf
hidebon0630 <hyagi0630@icloud.com>
2021-08-12 16:36:07 +0900

    HTTP/1.1のKeep-Aliveに対応

--- main.go
+++ main.go
@@ -3,30 +3,65 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"strings"
+	"time"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8888")
+	listener, err := net.Listen("tcp", "localhost:8888")
 	if err != nil {
 		panic(err)
 	}
-	request, err := http.NewRequest(
-		"GET", "http://localhost:8888", nil)
-	if err != nil {
-		panic(err)
+	fmt.Println("Server is running at localhost:8888")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go func() {
+			defer conn.Close()
+			fmt.Printf("Accept %v\n", conn.RemoteAddr())
+			// Accept後のソケットで何度も応答するためにループ
+			for {
+				// タイムアウトを設定
+				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+				// リクエストを読み込む
+				request, err := http.ReadRequest(bufio.NewReader(conn))
+				if err != nil {
+					// タイムアウトもしくはソケットクローズ時は終了
+					// それ以外はエラーにする
+					neterr, ok := err.(net.Error) // ダウンキャスト
+					if ok && neterr.Timeout() {
+						fmt.Println("Timeout")
+						break
+					} else if err == io.EOF {
+						break
+					}
+					panic(err)
+				}
+				// リクエストを表示
+				dump, err := httputil.DumpRequest(request, true)
+				if err != nil {
+					panic(err)
+				}
+				fmt.Println(string(dump))
+				content := "Hello World\n"
+				// レスポンスを書き込む
+				// HTTP/1.1かつContentLengthの設定が必要
+				response := http.Response{
+					StatusCode:    200,
+					ProtoMajor:    1,
+					ProtoMinor:    0,
+					ContentLength: int64(len(content)),
+					Body:          ioutil.NopCloser(strings.NewReader(content)),
+				}
+				response.Write(conn)
+			}
+		}()
 	}
-	request.Write(conn)
-	response, err := http.ReadResponse(
-		bufio.NewReader(conn), request)
-	if err != nil {
-		panic(err)
-	}
-	dump, err := httputil.DumpResponse(response, true)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(dump))
 }
