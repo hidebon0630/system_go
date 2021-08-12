ced291e7667339204aa67eb5809a8caad0e9fb52
hidebon0630 <hyagi0630@icloud.com>
2021-08-12 17:00:54 +0900

    Keep-Alive対応のHTTPクライアント

--- main.go
+++ main.go
@@ -3,65 +3,63 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"strings"
-	"time"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8888")
-	if err != nil {
-		panic(err)
+	sendMessages := []string{
+		"ASCII",
+		"PROGRAMMING",
+		"PLUS",
 	}
-	fmt.Println("Server is running at localhost:8888")
+	current := 0
+	var conn net.Conn = nil
+	// リトライ用にループで全体を囲う
 	for {
-		conn, err := listener.Accept()
+		var err error
+		// まだコネクションを張ってない / エラーでリトライ
+		if conn == nil {
+			// Dialから行ってconnを初期化
+			conn, err = net.Dial("tcp", "localhost:8888")
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Access: %d\n", current)
+		}
+		// POSTで文字列を送るリクエスト作成
+		request, err := http.NewRequest(
+			"POST",
+			"http://localhost:8888",
+			strings.NewReader(sendMessages[current]))
 		if err != nil {
 			panic(err)
 		}
-		go func() {
-			defer conn.Close()
-			fmt.Printf("Accept %v\n", conn.RemoteAddr())
-			// Accept後のソケットで何度も応答するためにループ
-			for {
-				// タイムアウトを設定
-				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-				// リクエストを読み込む
-				request, err := http.ReadRequest(bufio.NewReader(conn))
-				if err != nil {
-					// タイムアウトもしくはソケットクローズ時は終了
-					// それ以外はエラーにする
-					neterr, ok := err.(net.Error) // ダウンキャスト
-					if ok && neterr.Timeout() {
-						fmt.Println("Timeout")
-						break
-					} else if err == io.EOF {
-						break
-					}
-					panic(err)
-				}
-				// リクエストを表示
-				dump, err := httputil.DumpRequest(request, true)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println(string(dump))
-				content := "Hello World\n"
-				// レスポンスを書き込む
-				// HTTP/1.1かつContentLengthの設定が必要
-				response := http.Response{
-					StatusCode:    200,
-					ProtoMajor:    1,
-					ProtoMinor:    0,
-					ContentLength: int64(len(content)),
-					Body:          ioutil.NopCloser(strings.NewReader(content)),
-				}
-				response.Write(conn)
-			}
-		}()
+		err = request.Write(conn)
+		if err != nil {
+			panic(err)
+		}
+		// サーバーから読み込む。タイムアウトはここでエラーになるのでリトライ
+		response, err := http.ReadResponse(
+			bufio.NewReader(conn), request)
+		if err != nil {
+			fmt.Println("Retry")
+			conn = nil
+			continue
+		}
+		// 結果を表示
+		dump, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+		// 全部送信完了していれば終了
+		current++
+		if current == len(sendMessages) {
+			break
+		}
 	}
+	conn.Close()
 }
