d18b97fe0dc53b1ffc5ece7686a14b0242505a55
hidebon0630 <hyagi0630@icloud.com>
2021-08-12 16:08:26 +0900

    TCPソケットを使ったHTTPサーバー

--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
 )
 
 func main() {
-	// サイズが1より大きいチャネルを作成
-	signals := make(chan os.Signal, 1)
-	// SIGINT(Ctrl+C)を受け取る
-	signal.Notify(signals, syscall.SIGINT)
-
-	// シグナルがくるまで待つ
-	fmt.Println("Waiting SIGINT (CTRL+C)")
-	<-signals
-	fmt.Println("SIGINT arrived")
+	listener, err := net.Listen("tcp", "localhost:8888")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Server is running at localhost:8888")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go func() {
+			fmt.Printf("Accept %v\n", conn.RemoteAddr())
+			// リクエストを読み込む
+			request, err := http.ReadRequest(bufio.NewReader(conn))
+			if err != nil {
+				panic(err)
+			}
+			dump, err := httputil.DumpRequest(request, true)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(dump))
+			// レスポンスを書き込む
+			response := http.Response{
+				StatusCode: 200,
+				ProtoMajor: 1,
+				ProtoMinor: 0,
+				Body:       ioutil.NopCloser(strings.NewReader("Hello World\n")),
+			}
+			response.Write(conn)
+			conn.Close()
+		}()
+	}
 }
