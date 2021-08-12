fa447556562cf54d7e4ed6fa9e58ad2659139920
hidebon0630 <hyagi0630@icloud.com>
2021-08-12 18:46:32 +0900

    チャンク形式のサーバーの実装

--- main.go
+++ main.go
@@ -2,72 +2,59 @@ package main
 
 import (
 	"bufio"
-	"bytes"
-	"compress/gzip"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"strings"
-	"time"
 )
 
-// クライアントはgzipを受け入れ可能か？
-func isGZipAcceptable(request *http.Request) bool {
-	return strings.Index(
-		strings.Join(request.Header["Accept-Encoding"], ","),
-		"gzip") != -1
+// 青空文庫: ごんぎつねより
+// http://www.aozora.gr.jp/cards/000121/card628.html
+var contents = []string{
+	"これは、私わたしが小さいときに、村の茂平もへいというおじいさんからきいたお話です。",
+	"むかしは、私たちの村のちかくの、中山なかやまというところに小さなお城があって、",
+	"中山さまというおとのさまが、おられたそうです。",
+	"その中山から、少しはなれた山の中に、「ごん狐ぎつね」という狐がいました。",
+	"ごんは、一人ひとりぼっちの小狐で、しだの一ぱいしげった森の中に穴をほって住んでいました。",
+	"そして、夜でも昼でも、あたりの村へ出てきて、いたずらばかりしました。",
 }
 
 // 1セッションの処理をする
 func processSession(conn net.Conn) {
 	fmt.Printf("Accept %v\n", conn.RemoteAddr())
 	defer conn.Close()
+	// Accept後のソケットで何度も応答を返すためループ
 	for {
-		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 		// リクエストを読み込む
 		request, err := http.ReadRequest(bufio.NewReader(conn))
 		if err != nil {
-			neterr, ok := err.(net.Error)
-			if ok && neterr.Timeout() {
-				fmt.Println("Timeout")
-				break
-			} else if err == io.EOF {
+			if err == io.EOF {
 				break
 			}
 			panic(err)
 		}
+
+		// リクエストを表示
 		dump, err := httputil.DumpRequest(request, true)
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println(string(dump))
+
 		// レスポンスを書き込む
-		response := http.Response{
-			StatusCode: 200,
-			ProtoMajor: 1,
-			ProtoMinor: 1,
-			Header:     make(http.Header),
+		fmt.Fprintf(conn, strings.Join([]string{
+			"HTTP/1.1 200 OK",
+			"Content-Type: text/plain",
+			"Transfer-Encoding: chunked",
+			"", "",
+		}, "\r\n"))
+		for _, content := range contents {
+			bytes := []byte(content)
+			fmt.Fprintf(conn, "%x\r\n%s\r\n", len(bytes), content)
 		}
-		if isGZipAcceptable(request) {
-			content := "Hello World (gzipped)\n"
-			// コンテンツをgzip化して転送
-			var buffer bytes.Buffer
-			writer := gzip.NewWriter(&buffer)
-			io.WriteString(writer, content)
-			writer.Close()
-			response.Body = ioutil.NopCloser(&buffer)
-			response.ContentLength = int64(buffer.Len())
-			response.Header.Set("Content-Encoding", "gzip")
-		} else {
-			content := "Hello World\n"
-			response.Body = ioutil.NopCloser(
-				strings.NewReader(content))
-			response.ContentLength = int64(len(content))
-		}
-		response.Write(conn)
+		fmt.Fprintf(conn, "0\r\n\r\n")
 	}
 }
 
