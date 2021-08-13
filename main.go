9449aa7afe358bf0992cae1fc090f70f6f18768b
hidebon0630 <hyagi0630@icloud.com>
2021-08-13 17:25:37 +0900

    複数のパスからなる文字列を分解

--- main.go
+++ main.go
@@ -7,6 +7,17 @@ import (
 )
 
 func main() {
-	dir, name := filepath.Split(os.Getenv("GOPATH"))
-	fmt.Printf("Dir: %s, Name: %s\n", dir, name)
+	if len(os.Args) == 1 {
+		fmt.Printf("%s [exec file name]", os.Args[0])
+		os.Exit(1)
+	}
+	for _, path := range filepath.SplitList(os.Getenv("PATH")) {
+		execpath := filepath.Join(path, os.Args[1])
+		_, err := os.Stat(execpath)
+		if !os.IsNotExist(err) {
+			fmt.Println(execpath)
+			return
+		}
+	}
+	os.Exit(1)
 }
