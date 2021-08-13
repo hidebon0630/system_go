f2ba9f59a0afc57de1f999db8ca1adc625f071d2
hidebon0630 <hyagi0630@icloud.com>
2021-08-13 14:29:56 +0900

    udp サーバー側実装

--- main.go
+++ main.go
@@ -1,61 +1,29 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
-	"net/http"
-	"net/http/httputil"
 )
 
 func main() {
-	sendMessages := []string{
-		"ASCII",
-		"PROGRAMMING",
-		"PLUS",
-	}
-	var conn net.Conn = nil
-	var err error
-	requests := make([]*http.Request, 0, len(sendMessages))
-
-	conn, err = net.Dial("tcp", "localhost:8888")
+	fmt.Println("Server is running at localhost:8888")
+	conn, err := net.ListenPacket("udp", "localhost:8888")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Access\n")
 	defer conn.Close()
-
-	// リクエストだけ先に送る
-	for i := 0; i < len(sendMessages); i++ {
-		lastMessage := i == len(sendMessages)-1
-		request, err := http.NewRequest(
-			"GET",
-			"http://localhost:8888?message="+sendMessages[i],
-			nil)
-		if lastMessage {
-			request.Header.Add("Connection", "close")
-		} else {
-			request.Header.Add("Connection", "keep-alive")
-		}
-		err = request.Write(conn)
+	buffer := make([]byte, 1500)
+	for {
+		length, remoteAddress, err := conn.ReadFrom(buffer)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println("send: ", sendMessages[i])
-		requests = append(requests, request)
-	}
-
-	// レスポンスをまとめて受信
-	reader := bufio.NewReader(conn)
-	for _, request := range requests {
-		response, err := http.ReadResponse(reader, request)
+		fmt.Printf("Received from %v: %v\n",
+			remoteAddress, string(buffer[:length]))
+		_, err = conn.WriteTo([]byte("Hello from Server"),
+			remoteAddress)
 		if err != nil {
 			panic(err)
 		}
-		dump, err := httputil.DumpResponse(response, true)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(string(dump))
 	}
 }
