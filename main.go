612e9159755f344229fd2ddecc973376a1dd283a
hidebon0630 <hyagi0630@icloud.com>
2021-08-13 17:20:01 +0900

    パスを分割する

--- main.go
+++ main.go
@@ -7,5 +7,6 @@ import (
 )
 
 func main() {
-	fmt.Printf("Temp File Path: %s\n", filepath.Join(os.TempDir(), "temp.txt"))
+	dir, name := filepath.Split(os.Getenv("GOPATH"))
+	fmt.Printf("Dir: %s, Name: %s\n", dir, name)
 }
