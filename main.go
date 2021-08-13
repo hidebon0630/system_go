e6f81388304973fd0b13165ee534693773942787
hidebon0630 <hyagi0630@icloud.com>
2021-08-13 14:34:54 +0900

    udp クライアント実装

--- main.go
+++ main.go
@@ -6,24 +6,21 @@ import (
 )
 
 func main() {
-	fmt.Println("Server is running at localhost:8888")
-	conn, err := net.ListenPacket("udp", "localhost:8888")
+	conn, err := net.Dial("udp4", "localhost:8888")
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
-	buffer := make([]byte, 1500)
-	for {
-		length, remoteAddress, err := conn.ReadFrom(buffer)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Received from %v: %v\n",
-			remoteAddress, string(buffer[:length]))
-		_, err = conn.WriteTo([]byte("Hello from Server"),
-			remoteAddress)
-		if err != nil {
-			panic(err)
-		}
+	fmt.Println("Sending to server")
+	_, err = conn.Write([]byte("Hello from Client"))
+	if err != nil {
+		panic(err)
 	}
+	fmt.Println("Receiving from server")
+	buffer := make([]byte, 1500)
+	length, err := conn.Read(buffer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Received %s\n", string(buffer[:length]))
 }
