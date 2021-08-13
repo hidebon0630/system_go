8537a6e1f1757a25fc126e88c7eafade65f7abf2
hidebon0630 <hyagi0630@icloud.com>
2021-08-13 16:55:15 +0900

    ファイルシステム追記モード

--- main.go
+++ main.go
@@ -1,33 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"os"
 )
 
-// 新規作成
-func open() {
-	file, err := os.Create("textfile.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	io.WriteString(file, "New file content\n")
-}
-
 // 読み込み
-func read() {
-	file, err := os.Open("textfile.txt")
+func append() {
+	file, err := os.OpenFile("textfile.txt", os.O_RDWR|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
-	fmt.Println("Read file:")
-	io.Copy(os.Stdout, file)
+	io.WriteString(file, "Appended content\n")
 }
 
 func main() {
-	open()
-	read()
+	append()
 }
