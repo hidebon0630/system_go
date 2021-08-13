81178cde291b5e981698d53c66baf8434a0319a5
hidebon0630 <hyagi0630@icloud.com>
2021-08-13 14:41:30 +0900

    マルチキャストのサーバー実装修正

--- main.go
+++ main.go
@@ -3,24 +3,24 @@ package main
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
+const interval = 10 * time.Second
+
 func main() {
-	conn, err := net.Dial("udp4", "localhost:8888")
+	fmt.Println("Start tick server at 224.0.0.1:9999")
+	conn, err := net.Dial("udp", "224.0.0.1:9999")
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
-	fmt.Println("Sending to server")
-	_, err = conn.Write([]byte("Hello from Client"))
-	if err != nil {
-		panic(err)
+	start := time.Now()
+	wait := start.Truncate(interval).Add(interval).Sub(start)
+	time.Sleep(wait)
+	ticker := time.Tick(interval)
+	for now := range ticker {
+		conn.Write([]byte(now.String()))
+		fmt.Println("Tick: ", now.String())
 	}
-	fmt.Println("Receiving from server")
-	buffer := make([]byte, 1500)
-	length, err := conn.Read(buffer)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Received %s\n", string(buffer[:length]))
 }
