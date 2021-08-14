7b634e24aa249be0e7f368040cb5494c06ed2afe
hidebon0630 <hyagi0630@icloud.com>
2021-08-14 16:09:40 +0900

    プロセスグループとセッショングループのID取得

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"syscall"
 )
 
 func main() {
-	fmt.Printf("プロセスID: %d\n", os.Getpid())
-	fmt.Printf("親プロセスID: %d\n", os.Getppid())
+	sid, _ := syscall.Getsid(os.Getpid())
+	fmt.Fprintf(os.Stderr, "グループID: %d セッションID: %d\n", syscall.Getpgrp(), sid)
 }
