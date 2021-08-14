c21b18cd7b1c42a875b7f04f90b3679089657d83
hidebon0630 <hyagi0630@icloud.com>
2021-08-14 18:47:49 +0900

    goroutine

--- main.go
+++ main.go
@@ -5,18 +5,18 @@ import (
 	"time"
 )
 
-func sub1(c int) {
-	fmt.Println("share by arguments:", c*c)
-}
-
 func main() {
-	// 引数渡し
-	go sub1(10)
-
-	// クロージャのキャプチャ渡し
-	c := 20
-	go func() {
-		fmt.Println("share by capture", c*c)
-	}()
+	tasks := []string{
+		"cmake ..",
+		"cmake . --build Release",
+		"cpack",
+	}
+	for _, task := range tasks {
+		go func() {
+			// goroutineが起動するときにはループが回りきって
+			// 全部のtaskが最後のタスクになってしまう
+			fmt.Println(task)
+		}()
+	}
 	time.Sleep(time.Second)
 }
