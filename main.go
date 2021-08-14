81c20e4c539e3c7c6201a2c3ff0ea1f6d06470b7
hidebon0630 <hyagi0630@icloud.com>
2021-08-14 19:19:30 +0900

    sync.WaitGroup

--- main.go
+++ main.go
@@ -5,25 +5,27 @@ import (
 	"sync"
 )
 
-var id int
-
-func generateId(mutex *sync.Mutex) int {
-	// Lock()/Unlock()をペアで呼び出してロックする
-	mutex.Lock()
-	defer mutex.Unlock()
-	id++
-	return id
-}
-
 func main() {
-	// sync.Mutex構造体の変数宣言
-	// 次の宣言をしてもポインタ型になるだけで正常に動作する
-	// mutex := new(sync.Mutex)
-	var mutex sync.Mutex
+	var wg sync.WaitGroup
 
-	for i := 0; i < 100; i++ {
-		go func() {
-			fmt.Printf("id: %d\n", generateId(&mutex))
-		}()
-	}
+	// ジョブ数をあらかじめ登録
+	wg.Add(2)
+
+	go func() {
+		// 非同期で仕事をする(1)
+		fmt.Println("仕事1")
+		// Doneで完了を通知
+		wg.Done()
+	}()
+
+	go func() {
+		// 非同期で仕事をする(1)
+		fmt.Println("仕事2")
+		// Doneで完了を通知
+		wg.Done()
+	}()
+
+	// すべての処理が終わりのを待つ
+	wg.Wait()
+	fmt.Println("終了")
 }
