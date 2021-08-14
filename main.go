a0b09c6e2b4ac260ad41ff8172f7bc3b12454d92
hidebon0630 <hyagi0630@icloud.com>
2021-08-14 18:43:48 +0900

    goroutine

--- main.go
+++ main.go
@@ -1,35 +1,22 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/lestrrat/go-server-starter/listener"
+	"time"
 )
 
+func sub1(c int) {
+	fmt.Println("share by arguments:", c*c)
+}
+
 func main() {
-	// シグナル初期化
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGTERM)
+	// 引数渡し
+	go sub1(10)
 
-	// Server::Starterからもらったソケットを確認
-	listeners, err := listener.ListenAll()
-	if err != nil {
-		panic(err)
-	}
-	// ウェブサーバーをgoroutineで起動
-	server := http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "server pid: %d %v\n", os.Getpid(), os.Environ())
-		}),
-	}
-	go server.Serve(listeners[0])
-
-	// SIGTERMを受け取ったら終了させる
-	<-signals
-	server.Shutdown(context.Background())
+	// クロージャのキャプチャ渡し
+	c := 20
+	go func() {
+		fmt.Println("share by capture", c*c)
+	}()
+	time.Sleep(time.Second)
 }
