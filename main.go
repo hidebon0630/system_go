d71da36b83084b1c1e81dd85ad772d2ac10d02f5
hidebon0630 <hyagi0630@icloud.com>
2021-08-14 17:02:42 +0900

    countプログラムを起動し、標準出力に(stdout)というプリフィックスを付 けつつリアルタイムでリダイレクト

--- main.go
+++ main.go
@@ -1,23 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"os"
 	"os/exec"
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		return
-	}
-	cmd := exec.Command(os.Args[1], os.Args[2:]...)
-	err := cmd.Run()
+	count := exec.Command("./count")
+	stdout, _ := count.StdoutPipe()
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			fmt.Printf("(stdout) %s\n", scanner.Text())
+		}
+	}()
+	err := count.Run()
 	if err != nil {
 		panic(err)
 	}
-	state := cmd.ProcessState
-	fmt.Printf("%s\n", state.String())
-	fmt.Printf(" Pid: %d\n", state.Pid())
-	fmt.Printf(" System: %v\n", state.SystemTime())
-	fmt.Printf(" User: %v\n", state.UserTime())
 }
