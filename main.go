3af979d5ebf22b529d4eb34a78612ac5ab3e3713
hidebon0630 <hyagi0630@icloud.com>
2021-08-14 16:27:48 +0900

    作業フォルダ

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Printf("ユーザーID: %d\n", os.Getuid())
-	fmt.Printf("グループID: %d\n", os.Getgid())
-	fmt.Printf("実効ユーザーID: %d\n", os.Geteuid())
-	fmt.Printf("実効グループID: %d\n", os.Getegid())
+	wd, _ := os.Getwd()
+	fmt.Println(wd)
 }
