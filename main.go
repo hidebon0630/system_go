74871e3daa641c2abd070e07edd956ca5a2ce9d8
hidebon0630 <hyagi0630@icloud.com>
2021-08-14 16:04:00 +0900

    プロセスID取得

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-	path, _ := os.Executable()
-	fmt.Printf("実行ファイル名: %s\n", os.Args[0])
-	fmt.Printf("実行ファイルパス: %s\n", path)
+	fmt.Printf("プロセスID: %d\n", os.Getpid())
+	fmt.Printf("親プロセスID: %d\n", os.Getppid())
 }
