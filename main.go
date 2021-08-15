18e91fad1b25e9e20193566d59bac2224b7df798
hidebon0630 <hyagi0630@icloud.com>
2021-08-15 19:28:49 +0900

    現在時刻を取得

--- main.go
+++ main.go
@@ -2,22 +2,10 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"sync"
+	"time"
 )
 
 func main() {
-	var count int
-	pool := sync.Pool{
-		New: func() interface{} {
-			count++
-			return fmt.Sprintf("created: %d", count)
-		},
-	}
-
-	// GCを呼ぶと追加された要素が消える
-	pool.Put("removed 1")
-	pool.Put("removed 2")
-	runtime.GC()
-	fmt.Println(pool.Get())
+	t := time.Now()
+	fmt.Println(t.String())
 }
