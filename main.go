91edae52a822755185bcb58c0d6ff3bbe6b18b49
hidebon0630 <hyagi0630@icloud.com>
2021-08-15 19:16:33 +0900

    スライスのメモリ確保

--- main.go
+++ main.go
@@ -5,29 +5,18 @@ import (
 )
 
 func main() {
-	// 既存の配列を参照するスライス
-	a := [4]int{1, 2, 3, 4}
-	b := a[:]
-	fmt.Println("b:", &b[0], len(b), cap(b))
+	// 長さ1, 確保された要素2のスライスを作成
+	s := make([]int, 1, 2)
+	fmt.Println(&s[0], len(s), cap(s))
+	fmt.Println(s)
 
-	// 既存の配列の一部を参照するスライス
-	c := a[1:3]
-	fmt.Println("c:", &c[0], len(c), cap(c))
+	// 1要素追加
+	s = append(s, 100)
+	fmt.Println(&s[0], len(s), cap(s))
+	fmt.Println(s)
 
-	// 何も参照していないスライス
-	// 構造体のメンバー変数などではこの状態で未初期化にしておいてメモリを遅延確保するという使い方をする
-	var d []int
-	fmt.Println("d:", len(d), cap(d))
-	// 0 0
-
-	// 初期の配列とリンクされているスライス
-	e := []int{1, 2, 3, 4}
-	fmt.Println("e:", &e[0], len(e), cap(e))
-
-	// サイズを持ったスライスを定義
-	f := make([]int, 4)
-	fmt.Println("f:", &f[0], len(f), cap(f))
-
-	g := make([]int, 4, 8)
-	fmt.Println(&g[0], len(g), cap(g))
+	// 更に要素を追加（新しく配列が確保され直す）
+	s = append(s, 200)
+	fmt.Println(&s[0], len(s), cap(s))
+	fmt.Println(s)
 }
