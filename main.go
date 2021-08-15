cd28e838436b90dc74e119c4c4bf5ac4f5e4a56f
hidebon0630 <hyagi0630@icloud.com>
2021-08-15 19:39:24 +0900

    時刻のフォーマット

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
-	fmt.Println("waiting 5 seconds")
-	for now := range time.Tick(5 * time.Second) {
-		fmt.Println("now:", now)
-	}
+	now := time.Now()
+	fmt.Println(now.Format(time.RFC822))
+	// 15 Aug 21 19:37 JST
+
+	fmt.Println(now.Format("2006/01/02 03:04:05 MST"))
+	// 2021/08/15 07:37:51 JST
 }
