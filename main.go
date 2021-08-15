8b2eae2092e3c05fd390c120ac51bcbe101375f5
hidebon0630 <hyagi0630@icloud.com>
2021-08-15 19:36:05 +0900

    等間隔で通知

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fmt.Println("waiting 5 seconds")
-	after := time.After(5 * time.Second)
-	<-after
-	fmt.Println("done")
+	for now := range time.Tick(5 * time.Second) {
+		fmt.Println("now:", now)
+	}
 }
